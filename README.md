9fdaf457a12165286260f618fc53d9c219d0b1e6
jeyroik <jeyroik@gmail.com>
2019-10-02 13:12:21 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # extas-protocol-access
 Access protocol package for Extas
+
+Парсит параметры запроса и его заголовки на предмет наличия параметров доступа (секция, субъект и операция).
+
+Для заголовков префикс по умолчанию `x-extas-`, т.е. ожидаются следующие заголовки:
+- `x-extas-section`
+- `x-extas-subject`
+- `x-extas-operation`
+
+Переопределить префикс можно через переменную окружения `EXTAS__PROTOCOL_ACCESS__HEADER_PREFIX`.
---
+++ src/components/protocols/ProtocolExtasAccess.php
@@ -0,0 +1,81 @@
+<?php
+namespace extas\components\protocols;
+
+use extas\interfaces\access\IAccess;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Class ProtocolExtasAccess
+ *
+ * @package extas\components\protocols
+ * @author jeyroik@gmail.com
+ */
+class ProtocolExtasAccess extends Protocol
+{
+    const HEADER__PREFIX = 'x-extas-';
+
+    /**
+     * @param array $args
+     * @param RequestInterface $request
+     */
+    public function __invoke(array &$args = [], RequestInterface $request)
+    {
+        $accessEntities = [
+            IAccess::FIELD__SECTION, IAccess::FIELD__SUBJECT, IAccess::FIELD__OPERATION
+        ];
+
+        $fromHeaders = $this->grabHeaders($request, $accessEntities);
+        $fromParameters = $this->grabParameters($request, $accessEntities);
+
+        foreach ($accessEntities as $entity) {
+            if (isset($fromParameters[$entity])) {
+                $args[$entity] = $fromParameters[$entity];
+            } elseif (isset($fromHeaders[$entity])) {
+                $args[$entity] = $fromHeaders[$entity];
+            }
+        }
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $accessEntities
+     *
+     * @return array
+     */
+    protected function grabHeaders(RequestInterface $request, array $accessEntities)
+    {
+        $headerPrefix = getenv('EXTAS__PROTOCOL_ACCESS__HEADER_PREFIX') ?: static::HEADER__PREFIX;
+
+        $args = [];
+        foreach ($accessEntities as $entity) {
+            $headerName = $headerPrefix . $entity;
+            $headers = $request->getHeader($headerName);
+            if (count($headers)) {
+                $args[$entity] = array_shift($headers);
+            }
+        }
+
+        return $args;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $accessEntities
+     *
+     * @return array
+     */
+    protected function grabParameters(RequestInterface $request, array $accessEntities): array
+    {
+        parse_str($request->getUri()->getQuery(), $queryParams);
+
+        $args = [];
+
+        foreach ($accessEntities as $entity) {
+            if (isset($queryParams[$entity])) {
+                $args[$entity] = $queryParams[$entity];
+            }
+        }
+
+        return $args;
+    }
+}
